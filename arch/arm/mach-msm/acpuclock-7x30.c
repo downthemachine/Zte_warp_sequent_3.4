39cf938798cdee150455e93f019500df1e11217e
downthemachine <downthemachine@gmail.com>
2013-09-07 22:46:37 -0500

    acpu-clock7x30: UV edits

--- arch/arm/mach-msm/acpuclock-7x30.c
+++ arch/arm/mach-msm/acpuclock-7x30.c
@@ -122,11 +122,11 @@ static struct clk *acpuclk_sources[MAX_SOURCE];
  */
 static struct clkctl_acpu_speed acpu_freq_tbl[] = {
 	{ 0, 24576, LPXO, 0, 0,  30720000,  900, VDD_RAW(900) },
-	{ 1, 61440,   PLL_3, 5, 11, 61440000,  900, VDD_RAW(900) },
-	{ 1, 122880,  PLL_3, 5, 5,  61440000,  900, VDD_RAW(900) },
-	{ 0, 184320,  PLL_3, 5, 4,  61440000,  900, VDD_RAW(900) },
-	{ 0, MAX_AXI_KHZ, AXI, 1, 0, 61440000, 900, VDD_RAW(900) },
-	{ 1, 245760,  PLL_3, 5, 2,  61440000,  900, VDD_RAW(900) },
+	{ 1, 61440,   PLL_3, 5, 11, 61440000,  750, VDD_RAW(750) },
+	{ 1, 122880,  PLL_3, 5, 5,  61440000,  800, VDD_RAW(800) },
+	{ 0, 184320,  PLL_3, 5, 4,  61440000,  800, VDD_RAW(800) },
+	{ 0, MAX_AXI_KHZ, AXI, 1, 0, 61440000, 850, VDD_RAW(850) },
+	{ 1, 245760,  PLL_3, 5, 2,  61440000,  850, VDD_RAW(850) },
 	{ 1, 368640,  PLL_3, 5, 1,  122800000, 900, VDD_RAW(900) },
 	{ 0, 460800,  PLL_1, 2, 0,  153600000, 900, VDD_RAW(900) },
 	{ 0, 576000,  PLL_1, 2, 0,  153600000, 950, VDD_RAW(950) },
@@ -135,17 +135,17 @@ static struct clkctl_acpu_speed acpu_freq_tbl[] = {
 	/*
 	 * AXI has MSMC1 implications. See above.
 	 */
-	{ 1, 806400,  PLL_2, 3, 0, UINT_MAX, 1000, VDD_RAW(1000), &pll2_tbl[0]},
-	{ 1, 921600,  PLL_2, 3, 0, UINT_MAX, 1000, VDD_RAW(1000), &pll2_tbl[1]},
-	{ 1, 1024000, PLL_2, 3, 0, UINT_MAX, 1000, VDD_RAW(1000), &pll2_tbl[2]},
-	{ 0, 1113000, PLL_2, 3, 0, UINT_MAX, 1050, VDD_RAW(1050), &pll2_tbl[3]},
-	{ 1, 1209600, PLL_2, 3, 0, UINT_MAX, 1100, VDD_RAW(1100), &pll2_tbl[4]},
-	{ 0, 1305600, PLL_2, 3, 0, UINT_MAX, 1150, VDD_RAW(1150), &pll2_tbl[5]},
-	{ 1, 1401600, PLL_2, 3, 0, UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[6]},
-	{ 1, 1516800, PLL_2, 3, 0, UINT_MAX, 1250, VDD_RAW(1250), &pll2_tbl[7]},
-	{ 1, 1612800, PLL_2, 3, 0, UINT_MAX, 1350, VDD_RAW(1350), &pll2_tbl[8]},
-	{ 1, 1708800, PLL_2, 3, 0, UINT_MAX, 1400, VDD_RAW(1400), &pll2_tbl[9]},
-	{ 1, 1804800, PLL_2, 3, 0, UINT_MAX, 1450, VDD_RAW(1450), &pll2_tbl[10]},
+	{ 1, 806400,  PLL_2, 3, 0, UINT_MAX, 950, VDD_RAW(950), &pll2_tbl[0]},
+	{ 1, 921600,  PLL_2, 3, 0, UINT_MAX, 950, VDD_RAW(950), &pll2_tbl[1]},
+	{ 1, 1024000, PLL_2, 3, 0, UINT_MAX, 950, VDD_RAW(950), &pll2_tbl[2]},
+	{ 0, 1113000, PLL_2, 3, 0, UINT_MAX, 1000, VDD_RAW(1000), &pll2_tbl[3]},
+	{ 1, 1209600, PLL_2, 3, 0, UINT_MAX, 1000, VDD_RAW(1000), &pll2_tbl[4]},
+	{ 0, 1305600, PLL_2, 3, 0, UINT_MAX, 1050, VDD_RAW(1050), &pll2_tbl[5]},
+	{ 1, 1401600, PLL_2, 3, 0, UINT_MAX, 1100, VDD_RAW(1100), &pll2_tbl[6]},
+	{ 1, 1516800, PLL_2, 3, 0, UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[7]},
+	{ 1, 1612800, PLL_2, 3, 0, UINT_MAX, 1275, VDD_RAW(1275), &pll2_tbl[8]},
+	{ 1, 1708800, PLL_2, 3, 0, UINT_MAX, 1325, VDD_RAW(1325), &pll2_tbl[9]},
+	{ 1, 1804800, PLL_2, 3, 0, UINT_MAX, 1375, VDD_RAW(1375), &pll2_tbl[10]},
 	{ 0 }
 };
 
